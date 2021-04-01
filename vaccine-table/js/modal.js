a3f6f99a0d33b70b5f465b61cf482776b2f0ba03
Natasha Vicens <nkhan@publicsource.org>
2021-04-01 15:58:12 -0400

    added vaccine table

---
+++ vaccine-table/js/modal.js
@@ -0,0 +1,486 @@
+
+$(document).ready(function() {
+	      $("#modal-1").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-2").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-3").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-4").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-5").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-6").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-7").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-8").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-9").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-10").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-11").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-12").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-13").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-14").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-15").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-16").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-17").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-18").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-19").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-20").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-21").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-22").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-23").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-24").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-25").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-26").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
+
+$(document).ready(function() {
+          $("#modal-27").on("change", function() {
+            if ($(this).is(":checked")) {
+              $("body").addClass("modal-open");
+            } else {
+              $("body").removeClass("modal-open");
+            }
+          });
+
+          $(".modal-fade-screen, .modal-close").on("click", function() {
+            $(".modal-state:checked").prop("checked", false).change();
+          });
+
+          $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+          });
+        });
