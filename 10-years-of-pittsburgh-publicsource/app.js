ef5e42c442510a75fb990d58709d32f323b70dc5
Natasha Vicens <nkhan@publicsource.org>
2022-06-10 15:20:21 -0400

    added anniversary page

---
+++ 10-years-of-pittsburgh-publicsource/app.js
@@ -0,0 +1,28 @@
+(function(){
+var _$main_1 = {};
+// require("./lib/pym");
+// not using jquery
+window.onload = function () {
+  var story = document.getElementById('story-page-body');
+
+  if (story) {
+    var paragraphs = document.getElementById('story-page-body').getElementsByTagName('p');
+
+    for (var i = 0; i < paragraphs.length; i++) {
+      var anchors = paragraphs[i].getElementsByTagName('a');
+
+      for (var x = 0; x < anchors.length; x++) {
+        anchors[x].setAttribute('target', '_blank');
+      }
+    }
+  }
+}; // animate on scroll
+
+
+AOS.init({
+  easing: 'ease-in',
+  duration: 5000
+});
+
+}());
+//# sourceMappingURL=app.js.map
